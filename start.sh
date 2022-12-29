2b40186eb62d65e37096edb23ff33794b78d2f95
Gourav1255 <114558061+Gourav1255@users.noreply.github.com>
2022-12-29 19:10:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Sohanrazz/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Gourav1255/linksenderbotA11.git /linksenderbotA11
+cd /linksenderbotA11
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
